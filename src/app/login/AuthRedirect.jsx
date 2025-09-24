81494ab08af86a33c95ec0c628e9f5fb908e750a
TJsohn <sohntaejeong@gmail.com>
2025-09-24 13:18:17 +0300

    after lesson 2

---
+++ src/app/login/AuthRedirect.jsx
@@ -0,0 +1,18 @@
+import { useRouter } from "next/navigation";
+import { useAuth } from "../context/AuthContext"
+import { useEffect } from "react";
+
+export const AuthRedirect = () => {
+    const {user} = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (user) {
+            router.push("/protected");
+        }
+    }, [user, router]);
+
+    return null;
+};
+
+export default AuthRedirect;
